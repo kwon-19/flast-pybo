f048da56fd2ea2c6cc21c5121fa9d068b495cb6f
kwon-19 <ilku4569@naver.com>
2026-09-11 17:19:01 +0900

    260911:question/answer

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,13 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, redirect,url_for
 
 # Blueprint: 라우팅 함수를 체계적으로 관리
-
 bp = Blueprint('main', __name__, url_prefix='/')
 
 @bp.route('/')
-def hello_pybo():
-    return 'Hello Pybo~!'
-
-@bp.route('/hello')
-def hello():
-    return 'Hello page!'
+def index():
+    return redirect(url_for('question._list'))
